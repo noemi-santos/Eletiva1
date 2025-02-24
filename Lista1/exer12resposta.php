650319a6599fe4fcc781b2c40929f5dacdf5dabc
noemi-santos <149540792+noemi-santos@users.noreply.github.com>
2025-02-24 16:14:51 -0300

    Exercicio12_corrigido

--- Lista1/exer12resposta.php
+++ Lista1/exer12resposta.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Resposta do Exercício 11</h1>
+    <h1>Resposta do Exercício 12</h1>
     <?php
     if ($_SERVER['REQUEST_METHOD'] == 'POST')
         {
