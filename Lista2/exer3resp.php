8ab09cebefb5138052c0a3c2df1f1c444af46954
noemi-santos <149540792+noemi-santos@users.noreply.github.com>
2025-03-05 20:15:33 -0300

    Lista2_exer3

---
+++ Lista2/exer3resp.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>Resposta</h1>
+
+    <?php
+        if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+            try {
+
+                $valor1 = $_POST['valor1']; 
+                $valor2 = $_POST['valor2'];
+                $menor = $valor1;
+                $maior = $valor2;
+
+                
+                if ($valor2 < $menor){
+                    $menor = $valor2;
+                    $maior = $valor1;
+                    echo " Ordem crescente: $menor ; $maior";
+                }
+                
+                elseif ($valor1 == $valor2){
+                    echo "Números iguais: $valor1";
+                }
+                
+                else{
+                echo " Ordem crescente: $menor ; $maior";
+                }
+                
+            } catch (Exception $e) { 
+                echo $e->getMessage(); 
+        }
+    
+        }
