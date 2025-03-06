8a425698f9518f5e1ff72887d825d2a72e3a52b4
noemi-santos <149540792+noemi-santos@users.noreply.github.com>
2025-03-05 22:39:04 -0300

    Lista2_exer8

---
+++ Lista2/exer8resp.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>Resposta</h1>
+
+    <?php
+        if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+            try {
+
+                $valor1 = $_POST['valor1'];
+                $i = $valor1;
+                do{
+                    echo"$i<br/>";
+                    $i--;
+                }while($i >= 1);
+            
+                }
+
+                catch (Exception $e) { 
+                echo $e->getMessage(); 
+            }
+        }
