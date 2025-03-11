dc1ac0471f2a3483040a6ef4c329ce1971489f06
noemi-santos <149540792+noemi-santos@users.noreply.github.com>
2025-03-11 17:16:50 -0300

    Lista3_exer2

---
+++ Lista3/resp2.php
@@ -0,0 +1,47 @@
+<?php
+    declare(strict_types = 1); //obriga a tipagem da qualquer método ou atributo
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercício 1</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <h1>Resposta</h1>
+
+    <?php
+
+    function maisculo(string  $palavra): void {
+        echo "A palavra em maísculo é: " .strtoupper($palavra);
+    }
+
+    
+    function minusculo(string  $palavra): void {
+        echo "A palavra em minúsculo é: " .strtolower($palavra);
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+        try {
+            $palavra = $_POST["texto"];//pega qualquer texte e tranformar em um numero
+            maisculo($palavra);
+            minusculo($palavra);
+
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+    ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
