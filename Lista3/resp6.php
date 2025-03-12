fc6481ea9e07d6150ceff0331e1af14d3ad47a9c
noemi-santos <149540792+noemi-santos@users.noreply.github.com>
2025-03-12 15:50:58 -0300

    Lista3_exer6

---
+++ Lista3/resp6.php
@@ -0,0 +1,44 @@
+<?php
+    declare(strict_types = 1); //obriga a tipagem da qualquer método ou atributo
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercício 6</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <h1>Resposta</h1>
+
+    <?php
+
+    function arredondar(float $valor): float {
+        return round($valor);
+
+    }
+
+
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+        try {
+            $valor = floatval($_POST["valor"]);//pega qualquer texte e tranformar em um numero
+            arredondar($valor);
+            echo "O número arredondado é: ".arredondar($valor);
+
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+    ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
